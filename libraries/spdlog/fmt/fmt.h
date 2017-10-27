b8a7583211fd9323050879ad1c0314555652567c
Edward Tremel <edward@cs.cornell.edu>
2017-10-27 10:42:56 -0400

    Updated spdlog library to latest version

--- libraries/spdlog/fmt/fmt.h
+++ libraries/spdlog/fmt/fmt.h
@@ -18,11 +18,17 @@
 #ifndef FMT_USE_WINDOWS_H
 #define FMT_USE_WINDOWS_H 0
 #endif
-#include <spdlog/fmt/bundled/format.h>
+#include "spdlog/fmt/bundled/format.h"
+#if defined(SPDLOG_FMT_PRINTF)
+#include "spdlog/fmt/bundled/printf.h"
+#endif
 
 #else //external fmtlib
 
 #include <fmt/format.h>
+#if defined(SPDLOG_FMT_PRINTF)
+#include <fmt/printf.h>
+#endif
 
 #endif
 
